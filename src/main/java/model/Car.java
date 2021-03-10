b6862984c545e7a9d6dd26dffc7ebbc6e8f837bd
Pawel Janusz <marek80k@wp.pl>
2021-03-10 17:58:37 +0100

    first commit

---
+++ src/main/java/model/Car.java
@@ -0,0 +1,17 @@
+import lombok.*;
+
+import javax.persistence.Entity;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class Car {
+
+    private Long id;
+    private Long 
+
+}
