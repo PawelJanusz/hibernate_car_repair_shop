ac453f95ceeab62eeea946c7c507fcedcc2dbe3c
Pawel Janusz <marek80k@wp.pl>
2021-03-20 21:27:38 +0100

    test dependencies, refactor CarDao

--- src/main/java/dao/CarDao.java
+++ src/main/java/dao/CarDao.java
@@ -18,58 +18,7 @@ import java.util.Optional;
 public class CarDao {
 
     private SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-    private Transaction transaction = null;
 
-    public void saveOrUpdate(Car car){
-        try (Session session = sessionFactory.openSession()){
-            transaction = session.beginTransaction();
-            session.saveOrUpdate(car);
-            transaction.commit();
-        }catch (HibernateException ex){
-            if (transaction != null){
-                transaction.rollback();
-            }
-        }
-    }
-
-    public void delete(Car car){
-        try (Session session = sessionFactory.openSession()){
-            transaction = session.beginTransaction();
-            session.delete(car);
-            transaction.commit();
-        }catch (HibernateException ex){
-            if (transaction != null){
-                transaction.rollback();
-            }
-        }
-    }
-
-    public Optional<Car> findById(Long id) {
-        try (Session session = sessionFactory.openSession()){
-            return Optional.ofNullable(session.get(Car.class, id));
-        }catch (HibernateException ex){
-            ex.printStackTrace();
-        }
-        return Optional.empty();
-    }
-
-    public List<Car> getAll(){
-        List<Car> list = new ArrayList<>();
-
-        try (Session session = sessionFactory.openSession()){
-
-            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-            CriteriaQuery<Car> criteriaQuery = criteriaBuilder.createQuery(Car.class);
-            Root<Car> carRoot = criteriaQuery.from(Car.class);
-
-            criteriaQuery.select(carRoot);
-
-            list.addAll(session.createQuery(criteriaQuery).list());
-        }catch (HibernateException ex){
-            ex.printStackTrace();
-        }
-        return list;
-    }
 
     public List<Car> findByBrandName(Brand brand){
         List<Car> list = new ArrayList<>();
