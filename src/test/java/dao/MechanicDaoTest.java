9a02ea966d668ac1aa1ba96650d03fd80370d0be
Pawel Janusz <marek80k@wp.pl>
2022-01-06 17:41:17 +0100

    tests mechanicDao

--- src/test/java/dao/MechanicDaoTest.java
+++ src/test/java/dao/MechanicDaoTest.java
@@ -1,6 +1,7 @@
 package dao;
 
 import model.Mechanic;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -10,22 +11,60 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MechanicDaoTest {
 
+    private EntityDao<Mechanic> dao;
+    private MechanicDao mechanicDao;
+
+    @BeforeEach
+    void runBeforeEach() {
+        dao = new EntityDao<>();
+        mechanicDao = new MechanicDao();
+
+    }
+
     @Test
     void addedMechanicShouldBeFindByLastName(){
         //given
-        EntityDao<Mechanic> dao = new EntityDao<>();
-        MechanicDao mechanicDao = new MechanicDao();
         Mechanic mechanic = new Mechanic("Artur", "Ostrowski", true);
         //when
         dao.saveOrUpdate(mechanic);
         Optional<Mechanic> addedMechanic = dao.findById(Mechanic.class, 9L);
-        Optional<Mechanic> foundMechanic = mechanicDao.findByMechanicLastName("Ostrowski")
-                                                        .stream()
-                                                        .findFirst();
+        Optional<Mechanic> foundMechanic = mechanicDao
+                .findByMechanicLastName("Ostrowski")
+                .stream()
+                .findFirst();
         //then
-        assertEquals(addedMechanic, foundMechanic);
+        assertEquals(foundMechanic, addedMechanic);
     }
 
+    @Test
+    void addedMechanicShouldBeFindByFirstName(){
+        //given
+        Mechanic mechanic = new Mechanic("Tomek", "Morawski", true);
+        //when
+        dao.saveOrUpdate(mechanic);
+        Optional<Mechanic> addedMechanic = dao.findById(Mechanic.class, 14L);
+        Optional<Mechanic> foundMechanic = mechanicDao
+                .findByMechanicFirstName("Tomek")
+                .stream()
+                .findFirst();
+        //then
+        assertEquals(foundMechanic, addedMechanic);
+    }
+
+    @Test
+    void addedMechanicShouldBeFindByFirstAndLastName(){
+        //given
+        Mechanic mechanic = new Mechanic("Anna", "Kedra", true);
+        //when
+        dao.saveOrUpdate(mechanic);
+        Optional<Mechanic> addedMechanic = dao.findById(Mechanic.class, 22L);
+        Optional<Mechanic> foundMechanic = mechanicDao
+                .findByMechanicFirstAndLastName("Anna", "Kedra")
+                .stream()
+                .findFirst();
+        //then
+        assertEquals(foundMechanic, addedMechanic);
+    }
 
 
 }
